92f6fc878f31ed90ef46b51bcc24df0c735b3cb1
eunchong <dmschd92@naver.com>
2020-09-08 23:16:33 +0900

    first

---
+++ src/main/java/com/chong/webfluxpractice/WebfluxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.chong.webfluxpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chong/webfluxpractice/WebfluxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chong.webfluxpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
